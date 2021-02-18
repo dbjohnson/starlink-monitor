f65d98136c5b6802711d8531880a019336ea4b21
Bryan Johnson <d.bryan.johnson@gmail.com>
2021-02-17 22:13:58 -0500

    use multiple threads for speedtest

--- app/speedtest.py
+++ app/speedtest.py
@@ -3,12 +3,12 @@ import time
 import speedtest
 
 
-def test():
+def test(threads=8):
     s = speedtest.Speedtest()
     s.get_servers([])
     s.get_best_server()
     s.download(threads=None)
-    s.upload(threads=None)
+    s.upload(threads=8)
     return {
         **s.results.dict(),
         'timestamp': time.time()
