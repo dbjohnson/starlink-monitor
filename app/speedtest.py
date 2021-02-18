fded433dd1bf0f7bba1df888a8adbc007f8f96c1
Bryan Johnson <d.bryan.johnson@gmail.com>
2021-02-17 22:18:25 -0500

    fix speedtest threads

--- app/speedtest.py
+++ app/speedtest.py
@@ -7,8 +7,8 @@ def test(threads=8):
     s = speedtest.Speedtest()
     s.get_servers([])
     s.get_best_server()
-    s.download(threads=None)
-    s.upload(threads=8)
+    s.download(threads=threads)
+    s.upload(threads=threads)
     return {
         **s.results.dict(),
         'timestamp': time.time()
