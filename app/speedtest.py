ceadeec31467e266e45c9e916d300bffd327964e
Bryan Johnson <d.bryan.johnson@gmail.com>
2024-02-16 12:25:55 -0500

    blackify

--- app/speedtest.py
+++ app/speedtest.py
@@ -10,28 +10,29 @@ def test():
     gives a more consistent assessment of performance than speedtest-cli,
     which is limited to downloads < 5MB (from what I can tell)
     """
+
     def parse_line(line):
-        key, val = [s.strip() for s in line.split(':')]
+        key, val = [s.strip() for s in line.split(":")]
         try:
-            return key, float(val.split(' ')[0])
+            return key, float(val.split(" ")[0])
         except ValueError:
             return key, val
 
     test_results = re.sub(
         # regex for stripping ansi color escape codes.
-        r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]',
-        '',
+        r"(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]",
+        "",
         subprocess.getoutput(
-            'npx speed-cloudflare-cli',
-        )
+            "npx speed-cloudflare-cli",
+        ),
     )
 
-    return {**{
-        key: val
-        for line in
-        test_results.split('\n')
-        if ':' in line
-        for key, val in [parse_line(line)]
-    },
-        'timestamp': time.time()
+    return {
+        **{
+            key: val
+            for line in test_results.split("\n")
+            if ":" in line
+            for key, val in [parse_line(line)]
+        },
+        "timestamp": time.time(),
     }
